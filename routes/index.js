6bacd487cf073bafc50d5bf4923580bcd595557c
Andrew <avarnell@ago.org>
2015-12-28 13:37:42 -0700

    added innerjoins

--- routes/index.js
+++ routes/index.js
@@ -8,10 +8,38 @@ var knex = require('knex')(require('../knexfile')['development'])
 router.get('/', function(req, res, next) {
   knex('users').then(function(users){
     res.render('index', { 
-      title: 'Express',
+      title: 'weConnect',
       users : users 
     });
   })
 });
+
+router.get('/users/:id', function(req,res,next){
+  knex('users').where({id: req.params.id}).first().then(function(user){
+
+    knex('connections')
+      .innerJoin('users', 'other_id', 'users.id')
+      .where({user_id : user.id})
+      .then(function(connections){
+
+        knex('connections')
+          .where({other_id : user.id})
+          .innerJoin('users', 'user_id', 'users.id')
+          .then(function(connections2){
+
+            
+
+            res.render('user', {user:user,
+              connections: connections.concat(connections2)
+            })
+
+          })
+      })
+
+
+
+
+  })
+})
 
 module.exports = router;
